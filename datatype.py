7fc1846ca3c17c90db2015311250016ac8d76bea
unknown <esthermutuan@gmail.com>
2024-03-07 16:15:12 +0300

    initial commit

---
+++ datatype.py
@@ -0,0 +1,42 @@
+a=100
+b=3.14
+#print(a*b)
+c=(5+7j)
+#print(a+b-c)
+d=False
+#print(d)
+#print(a/b)
+str="School mid term"
+#print(str)# start of string
+#print(str[0])# to get from a string values as the first letter is 0 and the last -1
+#print(str[-1])
+#print(str[4:10])# to get a range of letters
+plp="python class"
+#print(plp+str)
+#print(plp * 4) # to do repetition of code that is the output that is all for strings
+# LIST in python it allows any type of data type elemnts can be changed and enclosed in[]
+Shopping_list=["bread","milk","soap",20,40.5,5+8j]
+#print(Shopping_list)
+Building_list=["cement","sand",450,'wheelbarrow',180,'shovels',"bread"]
+#print(Building_list)
+#print(Shopping_list[0])
+#print(Shopping_list[2:4])
+#print(Shopping_list + Building_list)
+#print(Shopping_list * 3)
+#TUPLE enlosed in () elements cannot be changed
+plp_tuple = ("abcd", 786, 2.23, "john", 7.02)
+#print(plp_tuple)
+tiny_tuple = ("john", 5+7j)
+#print(tiny_tuple)
+#print(plp_tuple + tiny_tuple)
+#PYTHON DICTIONARY DATATYPE we use{} and the : to separe a key and a value
+dict= {}
+dict['one']= "this is one"
+dict[2]="this is two"
+tinydict={"name":"john","code":6734,"dept":1007}
+print(dict["one"])
+print(dict[2])
+print(tinydict)
+print(tinydict.keys())# name,code,dept
+print(tinydict.values())# john,6734,1007
+
---
+++ input.py
@@ -0,0 +1,6 @@
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+County = input("What is your county?")
+print(f"Hello {name}, you are {age} years old live in {location} at {County}.")
